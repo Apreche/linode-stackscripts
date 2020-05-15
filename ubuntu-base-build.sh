7049e4f0af8cdab60bb2dcfd054a636155c55fb7
Scott Rubin <apreche@frontrowcrew.com>
2020-05-14 21:45:38 -0400

    Complete Correct Ubuntu Base

---
+++ ubuntu-base-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+  
+# Log all StackScript output to a file
+exec > >(tee -i /var/log/stackscript.log)
+
+# StackScript recursiveness doesn't work.
+# Copy all ssinclude and UDF statements to top level script
+source <ssinclude StackScriptID=1>
+source <ssinclude StackScriptID=644166>
+
+# <UDF name="hostname" label="Node Hostname" default="localhost" example="mysite.com" />
+# <UDF name="username" label="Non-root username" default="apreche" example="apreche" />
+# <UDF name="password" label="Password ofr non-root user" />
+# <UDF name="ssh_pubkey" label="SSH public key for non-root user" />
+# <UDF name="timezone" label="Node default timezone" default="America/New_York" example="America/New_York" />
+
+# Install base ubuntu image and nothing else
+install_ubuntu_base "$HOSTNAME" "$USERNAME" "$PASSWORD" "$TIMEZONE" "$SSH_PUBKEY"
--- ubuntu-base-include.sh
+++ ubuntu-base-include.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# Include the Linode StackScript Library
-Source <ssinclude StackScriptID=1>
+# *Copy this line to the upper level StackScript because they don't do it recursively
+source <ssinclude StackScriptID=1>
 
 function download_config {
-    # Download a config file from this repo to the local file system
+    # Utility function to download a config file from this repo to the local system
+
     # ${1} the name of the config file
     # ${2} the directory to put the config file in with trailing /
     # $3 chmod permissions for the config file
@@ -14,19 +15,21 @@ function download_config {
     local -r full_url="${base_url}${filename}"
     local -r config_path="${2}"
     local -r permissions=$3
-
     wget "${full_url}" --output-document="${config_path}${filename}"
-    chmod $3 ${config_path}
+    chmod $3 "${config_path}"
 }
 
 function update_and_upgrade {
     # Update and upgrade all packages
+
     system_update
     debian_upgrade
 }
 
 function enable_unattended_upgrades {
-    apt install unattended-upgrades
+    # Enable the unattended upgrades system on Ubuntu
+
+    apt-get install -y unattended-upgrades
     echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
     dpkg-reconfigure -f noninteractive unattended-upgrades
     download_config "50unattended-upgrades" "/etc/apt/apt.conf.d/" 644
@@ -34,68 +37,57 @@ function enable_unattended_upgrades {
 }
 
 function enable_mosh {
-    apt install mosh
+    # Enable the mosh shell
+
+    apt-get install -y mosh
     ufw allow 60000:61000/udp
 }
 
 function enable_ssh {
+    # Create a new user with an SSH pub key and configure ssh server
+
     # ${1} - username
     # ${2} - ssh public key
-
     local -r username="${1}"
     local -r ssh_pubkey="${2}"
-
     user_add_pubkey "${username}" "${ssh_pubkey}"
     ssh_disable_root
     ufw allow ssh
     enable_mosh
     enable_fail2ban
     service ssh restart
-
 }
 
 function enable_ufw {
+    # Enable the Ubuntu Uncomplicated FireWall
     ufw default deny incoming
     ufw default allow outgoing
-    ufw enable
+    ufw --force enable
 }
 
 function install_ubuntu_base {
-    # Performs setup common to all Ubuntu nodes
+    # Performs setup and configuration common to all Ubuntu nodes
 
     # ${1} - hostname
     # ${2} - non-root username
     # ${3} - non-root password
-    # ${4} - non-root SSH public key
-    # ${5} - timezone (America/New_York)
+    # ${4} - timezone (America/New_York)
+    # ${5} - non-root SSH public key
 
     local -r hostname="${1}"
     local -r username="${2}"
     local -r password="${3}"
-    local -r ssh_pubkey="${4}"
-    local -r timezone="${5}"
-
+    local -r timezone="${4}"
+    local -r ssh_pubkey="${5}"
 
     update_and_upgrade
-
     system_set_hostname "${hostname}"
     system_set_timezone "${timezone}"
-
-    # setup unattended upgrades
-    enabled_unattended_upgrades
-
-    # Set user
+    enable_unattended_upgrades
     user_add_sudo "${username}" "${password}"
     enable_passwordless_sudo "${username}"
-
-    enable_ssh
-
-    # setup local email
+    enable_ssh "${username}" "${ssh_pubkey}"
     postfix_install_loopback_only
-
-    # setup firewall
     enable_ufw
-
-    # END
     all_set
 }
